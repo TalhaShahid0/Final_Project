19dac5f883386c97d5840109aa734d564f3456be
FahaTariq <116685132+FahaTariq@users.noreply.github.com>
2022-12-09 04:31:21 +0500

    Fahad Tariq
    Chat Application

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
-const io=require('socket.io')(5000);
+const express=require('express');
+const http=require('http');
+const WebSocket=require('ws');
 
-io.on('connection',socket=>
-{
-    console.log('new User');
-    socket.emit('chat-message','Hello World')
+const port=6969;
+const server=http.createServer(express);
+const wss=new WebSocket.Server({server});
+wss.on('connection',function connection(ws)
+{ 
+ ws.on('message',function incoming(data)
+ {
+    wss.clients.forEach(function each(client)
+    {
+        if(client != ws && client.readyState == WebSocket.OPEN)
+        {
+            client.send(data);
+        }
+    })
+ })   
+})
+
+server.listen(port,function(){
+    console.log(`Server is listening on ${port}!`)
 })
